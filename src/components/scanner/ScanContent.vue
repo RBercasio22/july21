9fda700180f7c3b35447a87a2b98147627213c04
Red Hat Guy <RBercasio22@gmail.com>
2024-07-22 06:28:26 +0800

    remove comments

--- src/components/scanner/ScanContent.vue
+++ src/components/scanner/ScanContent.vue
@@ -13,21 +13,21 @@
         <v-row no-gutters>
             <v-col cols="3" style="background-color: rgb(33,33,33)">
                 <!--set background rgb(33,33,33)-->
-                <!--NO CONTENT ITS JUST A BLANK-->
+                <!--NO CONTENT-->
             </v-col>
             <v-col cols="6">
                 <v-sheet class="pa-2 text-center font-weight-light">Analyse suspicious files, domains, IPs and URLs to detect malware and other breaches, automatically share them with the <strong>security community.</strong></v-sheet>
             </v-col>
             <v-col cols="3" style="background-color: rgb(33,33,33)">
               <!--set background rgb(33,33,33)-->
-               <!--NO CONTENT ITS JUST A BLANK-->
+               <!--NO CONTENT-->
             </v-col>
 
 
               <!--INSERT A SCANBOTTOMNAVIGATION MENU-->
             <v-col cols="2" style="background-color: rgb(33,33,33)">
                 <!--set background rgb(33,33,33)-->
-                <!--NO CONTENT ITS JUST A BLANK-->
+                <!--NO CONTENT-->
             </v-col>
             <v-col cols="8">
                 <v-sheet class="pa-6"><ScanBottomNav /></v-sheet>
@@ -35,13 +35,13 @@
             </v-col>
             <v-col cols="2" style="background-color: rgb(33,33,33)">
                 <!--set background rgb(33,33,33)-->
-                <!--NO CONTENT ITS JUST A BLANK-->
+                <!--NO CONTENT-->
             </v-col>
             <!--INSERT ANOTHER FUNCTION AND IMPLEMENT ROUTING-->
               <!--INSERT A SCANBOTTOMNAVIGATION MENU-->
               <v-col cols="2" style="background-color: rgb(33,33,33)">
                 <!--set background rgb(33,33,33)-->
-                <!--NO CONTENT ITS JUST A BLANK-->
+                <!--NO CONTENT-->
             </v-col>
             <v-col cols="8">
                 <v-sheet class="text-center pa-2">
@@ -50,7 +50,7 @@
             </v-col>
             <v-col cols="2" style="background-color: rgb(33,33,33)">
                 <!--set background rgb(33,33,33)-->
-                <!--NO CONTENT ITS JUST A BLANK-->
+                <!--NO CONTENT -->
             </v-col>
 
         </v-row>
